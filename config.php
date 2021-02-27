879e07935323b7b8a48cab5fe1fbadd3e15a04c7
root <root@ip-10-0-1-154.ap-southeast-1.compute.internal>
2021-02-27 03:28:25 +0000

    first commit

--- config.php
+++ config.php
@@ -1,14 +1,13 @@
 <?php
-
-$db_host = "localhost";
-$db_user = "iqbal";
-$db_pass = "password";
-$db_name = "dbsosmed";
-
-try {    
-    //create PDO connection 
+$db_host = getenv("DB_HOST");
+$db_user = getenv("DB_USER");
+$db_pass = getenv("DB_PASS");
+$db_name = getenv("DB_NAME");
+try {
+    //create PDO connection
     $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 } catch(PDOException $e) {
     //show error
     die("Terjadi masalah: " . $e->getMessage());
 }
+?>
